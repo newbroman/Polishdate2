f9d22286c951a0f126722b8aa6e95de3beb69587
newbroman <105666230+newbroman@users.noreply.github.com>
2026-01-08 00:12:19 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
  * Handles offline caching for standalone app performance.
  */
 
-const CACHE_NAME = 'pl-date-v510';
+const CACHE_NAME = 'pl-date-v520';
 
 // All files required for the app to function offline
 const ASSETS = [
