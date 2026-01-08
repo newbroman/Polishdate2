45d5f8a1faf6a3433c33f3b88af9cb7d841be8eb
newbroman <105666230+newbroman@users.noreply.github.com>
2026-01-08 23:55:19 +0000

    Update ui-renderer.js

--- ui-renderer.js
+++ ui-renderer.js
@@ -95,10 +95,3 @@ export function speakPolish() {
     window.speechSynthesis.speak(utterance);
 }
 
-function getEnglishSuffix(i) {
-    const j = i % 10, k = i % 100;
-    if (j == 1 && k != 11) return "st";
-    if (j == 2 && k != 12) return "nd";
-    if (j == 3 && k != 13) return "rd";
-    return "th";
-}
