0d0016e1567fc2e1b5070685fdb8fba101e6e9cc
newbroman <105666230+newbroman@users.noreply.github.com>
2026-01-10 01:37:00 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -18,11 +18,14 @@ const ASSETS = [
 ];
 
 // Install Event
-self.addEventListener('install', event => {
+self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(ASSETS))
-      .then(() => self.skipWaiting())
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME)
+            .map(key => caches.delete(key))
+      );
+    }).then(() => self.clients.claim()) // Add this line!
   );
 });
 
